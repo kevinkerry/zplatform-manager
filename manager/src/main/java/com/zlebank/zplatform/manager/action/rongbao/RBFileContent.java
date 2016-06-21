e30e8fd5c3968587626a285a7bf4822144336bb6
Administrator <Administrator@WIN-20160603EAL>
2016-06-21 18:13:08 +0800

    融宝的对账文件修改了，所以修改读取excel的方法

--- manager/src/main/java/com/zlebank/zplatform/manager/action/rongbao/RBFileContent.java
+++ manager/src/main/java/com/zlebank/zplatform/manager/action/rongbao/RBFileContent.java
@@ -5,7 +5,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import jxl.Cell;
@@ -15,7 +19,6 @@ import jxl.read.biff.BiffException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import com.zlebank.zplatform.manager.action.upload.AbstractFileContentHandler;
 import com.zlebank.zplatform.manager.dao.object.BnkTxnModel;
 import com.zlebank.zplatform.manager.exception.ResolveReconFileContentException;
@@ -51,45 +54,51 @@ public class RBFileContent extends AbstractFileContentHandler {
     }
 
     public List<BnkTxnModel> getExcelToElement(Sheet rs, String instiid) {
-        bnkTxnList = new ArrayList<BnkTxnModel>();
-        String acqsettledate = "";
+        bnkTxnList = new ArrayList<BnkTxnModel>();       
         int row = rs.getRows();
-        Cell c1 = null, c2 = null, c3 = null, c4 = null, c5 = null ;
-
+        Cell c1,c2,c4,c5,c7 = null;
         if (row == 0) {
             return null;
         }
-        for (int k = 1; k < row; k++) {
-            if (k == 2) {
-                c2 = rs.getCell(1, k);// 商户订单号
-                acqsettledate = c2.getContents().trim();
-            }
-            if (k >= 7) {
-                // Cell[] cells = rs.getRow(k);
-                c1 = rs.getCell(1, k);// 流水号
-                c2 = rs.getCell(2, k);// 商户订单号
-                c3 = rs.getCell(3, k);// 支付时间
-                c4 = rs.getCell(4, k);// 交易类型
-                c5 = rs.getCell(5, k);// 交易金额
-                // UserModel user = getCurrentUser();
-                BnkTxnModel bnk = new BnkTxnModel();
-                bnk.setAcqsettledate(c1.getContents().trim());
-                bnk.setPaytrcno(c1.getContents().trim());
-                bnk.setPayordno(c2.getContents().trim());
-                bnk.setInstiid(instiid);
-                String txndatetime = c3.getContents().trim().toString();
-                bnk.setTxndatetime(txndatetime.replace(".", "")
-                        .replace(":", "").replace(" ", ""));
-                bnk.setBusicode(c4.getContents().trim());
-                bnk.setAcqsettledate(acqsettledate.replace("-", ""));
-                if (c5.getContents() != null
-                        && !c5.getContents().trim().equals("")) {
-                    bnk.setAmount(new BigDecimal(c5.getContents()).multiply(
-                            new BigDecimal(100)).longValue());
+        for (int k = 1; k < row; k++) {           
+            if (k >= 2) {
+                c1 = rs.getCell(1, k);// 商户订单号
+                c2 = rs.getCell(2, k);// 融宝交易号                
+                c4 = rs.getCell(4, k);// 支付完成时间（交易时间）
+                c5 = rs.getCell(5, k);// 订单金额
+                c7 = rs.getCell(7, k);// 手续费
+                BnkTxnModel bnk = new BnkTxnModel();              
+                bnk.setPayordno(c1.getContents().trim());
+                bnk.setSystrcno(c2.getContents().trim());              
+                if(c4.getContents().trim()!= null&& !c4.getContents().trim().equals("")){
+                    String txndatetime = c4.getContents().trim().toString();
+                    bnk.setTxndatetime(txndatetime.replace("-", "")
+                            .replace(":", "").replace(" ", ""));                  
+                }   
+                //受理清算日期               
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd"); 
+                Date date = null; 
+                try { 
+                 date = format.parse(c4.getContents().trim().toString()); 
+                } catch (ParseException e) { 
+                 e.printStackTrace(); 
+                }     
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                calendar.add(calendar.DATE,1);
+                date=calendar.getTime();
+                SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+                String str = sd.format(date).replace("-", "");
+                bnk.setAcqsettledate(str);
+                
+                if(c5.getContents() != null && !c5.getContents().trim().equals("")){
+                    bnk.setAmount(new BigDecimal(c5.getContents()).multiply(new BigDecimal(100)).longValue());
+                }
+                if(c7.getContents() != null && !c7.getContents().trim().equals("")){
+                    bnk.setCfee(new BigDecimal(c7.getContents()).multiply(new BigDecimal(100)).longValue());  
                 }
                 bnk.setRetcode("00");
-                bnk.setSystrcno(c1.getContents().trim());
-                bnkTxnList.add(bnk);
+                bnkTxnList.add(bnk); 
             }
         }
         return bnkTxnList;
